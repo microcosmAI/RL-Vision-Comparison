ce79f8b835b91f52983f160a41b63aec27ce69fb
syntactic <timothyho89@ucla.edu>
2023-10-16 17:48:19 +0200

    [Custom CNN] Fixed default model handling
    There was a bug where the ResNet50 model/weights wouldn't get loaded
    when no model was specified to the PPO agent. This should be fixed now.
    
    Also, in the script I've commented out the section that uses the vision
    transformer. In the notebook I've made a note about OOM issues using the
    vision transformer.

--- CustomCNN.py
+++ CustomCNN.py
@@ -1,24 +1,11 @@
 import torch as th
 import torch.nn as nn
-import tensorflow as tf
-import keras
-from tensorflow.keras.applications.xception import preprocess_input
+import torchvision
 from gymnasium import spaces
-
 from stable_baselines3 import PPO
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
-def convert_image_tensor_torch_to_tf(tensor_torch: th.Tensor) -> tf.Tensor:
-    tensor_torch_permuted = tensor_torch.permute(0, 2, 3, 1)
-    np_array = tensor_torch_permuted.numpy()
-    tensor_tf = tf.convert_to_tensor(np_array)
-    return tensor_tf
-
-def convert_tensor_tf_to_torch(tensor_tf: tf.Tensor) -> th.Tensor:
-    tensor_np = tensor_tf.numpy()
-    tensor_torch = th.from_numpy(tensor_np)
-    return tensor_torch
-
+device = th.device("cuda:0" if th.cuda.is_available() else "cpu")
 
 class CustomCNN(BaseFeaturesExtractor):
 
@@ -26,28 +13,27 @@ class CustomCNN(BaseFeaturesExtractor):
     def model(self):
         return self._model
 
+    @property
+    def weights(self):
+        return self._weights
+
+    @weights.setter
+    def weights(self, weights):
+        if weights is None:
+            print("Defaulting to using ResNet50 default weights.")
+            weights = torchvision.models.ResNet50_Weights.DEFAULT
+        self._weights = weights
+        self.preprocessing_function = weights.transforms()
+
     @model.setter
     def model(self, base_model):
         if base_model is None:
-            print("Defaulting to using Xception model.")
-            base_model = tf.keras.applications.Xception(
-                include_top=False,
-                input_shape=(299,299,3),
-                weights="imagenet",
-                input_tensor=None,
-                pooling=None,
-                classes=1000,
-                classifier_activation="softmax",
-            )
-        base_model.trainable = False
+            print("Defaulting to using ResNet50 model.")
+            base_model = torchvision.models.resnet50(self.weights)
+            base_model.fc = nn.Identity()
 
-        inputs = keras.Input(shape=(299, 299, 3))
-        with_inputs = base_model(inputs, training=False)
-        with_pooling = keras.layers.GlobalAveragePooling2D()(with_inputs)
-        outputs = keras.layers.Dense(self.features_dim, activation='relu')(with_pooling)
-
-        self._model = keras.Model(inputs, outputs)
-        print(f"Using {base_model.name} as the base model.")
+        self._model = base_model
+        print(f"Using {base_model._get_name()} as the base model.")
 
     @property
     def preprocessing_function(self):
@@ -56,51 +42,51 @@ class CustomCNN(BaseFeaturesExtractor):
     @preprocessing_function.setter
     def preprocessing_function(self, preprocessing_function):
         if preprocessing_function is None:
-            preprocessing_function = tf.keras.applications.xception.preprocess_input
+            preprocessing_function = self.weights.transforms()
         self._preprocessing_function = preprocessing_function
-        print(f"Using {preprocessing_function.__qualname__} as preprocessing function.")
 
     """
     :param observation_space: (gym.Space)
     :param features_dim: (int) Number of features extracted.
-        This corresponds to the number of unit for the last layer.
+        This corresponds to the number of units for the last layer.
+    :param base_model: PyTorch model
+    :param weights: PyTorch weights for the model
     """
-    def __init__(self, observation_space: spaces.Box, features_dim: int = 256, base_model = None, preprocessing_function = None):
+    def __init__(self, observation_space: spaces.Box, features_dim: int, base_model = None, weights = None):
 
         super().__init__(observation_space, features_dim)
 
+        self.weights = weights
         self.model = base_model
 
-        self.preprocessing_function = preprocessing_function
-
     def forward(self, observations: th.Tensor) -> th.Tensor:
-        observations_tf = convert_image_tensor_torch_to_tf(observations)
-        resized_observations = tf.image.resize(observations_tf, [299, 299])
-        preprocessed_observations = self.preprocessing_function(resized_observations)
+        observations = observations.to(device)
+        preprocessed_observations = self.preprocessing_function(observations)
         called = self.model(preprocessed_observations)
-        called_torch = convert_tensor_tf_to_torch(called)
-        return called_torch
+        return called
 
 
 if __name__ == "__main__":
-    """Testing with ResNet101. If a model isn't supplied in the policy_kwargs, the Xception
-        model from tf.keras.applications is used as the default model."""
+    """Testing with ViT_L_16. If a model isn't supplied in the policy_kwargs, the ResNet50
+        model is used as the default model."""
+    """vit_l_16_weights = torchvision.models.ViT_L_16_Weights.DEFAULT
+    vit_l_16_model = torchvision.models.vit_l_16(weights=vit_l_16_weights).to(device)
+    vit_l_16_model.heads = nn.Identity()
+    print(vit_l_16_model)
 
-    ResNet101 = tf.keras.applications.ResNet101(
-        include_top=False,
-        weights="imagenet",
-        input_tensor=None,
-        input_shape=(299,299,3),
-        pooling=None,
-        classes=1000
-    )
+    rand_input = th.rand(1,3,224,224).to(device)
+    with th.no_grad():
+        output = vit_l_16_model(rand_input)
+        output_dim = output.shape
+    num_features = output_dim[1]
+    print(f"{num_features} output units at the end of the vision model")"""
 
     policy_kwargs = dict(
         features_extractor_class=CustomCNN,
-        features_extractor_kwargs=dict(features_dim=128, base_model=ResNet101, 
-                                       preprocessing_function=tf.keras.applications.resnet.preprocess_input),
+        features_extractor_kwargs=dict(features_dim=2048),
     )
 
     model = PPO("CnnPolicy", "BreakoutNoFrameskip-v4", policy_kwargs=policy_kwargs, verbose=1)
-    model.learn(4000)
-    model.save("4ktimesteps.zip")
+    timesteps = 400000
+    model.learn(timesteps)
+    model.save(f"{timesteps}_timesteps.zip")
