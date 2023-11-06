d4aed8fa6fb06ede65fb5139b4c142ec939c8b19
syntactic <timothyho89@ucla.edu>
2023-11-06 17:47:51 +0100

    Updated CustomCNN to use make_atari_env from the SB3 library. Since this function makes the observations grayscale, I needed to re-expand the tensors to have 3 channels to work with pretrained CNNs expecting 3-channel images.
     Added hyperparameter search function to search over learning rate,
    batch size, and network architecture. The values in the limited search
    space and the values for arguments to PPO are set to values that I took
    from this model's training:
    https://huggingface.co/ThomasSimonini/ppo-BreakoutNoFrameskip-v4
    
    I was able to get a model obtaining between 6 and 7 reward after 3M
    timesteps of training.

--- CustomCNN.py
+++ CustomCNN.py
@@ -8,9 +8,44 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import VecFrameStack, VecEnvWrapper, DummyVecEnv
+from stable_baselines3.common.env_util import make_atari_env
 from HParamCallback import HParamCallback
 
 device = th.device("cuda:0" if th.cuda.is_available() else "cpu")
+
+def hyperparam_search(lr_values, batch_size_values, net_arch_values, policy_kwargs, timesteps):
+    for lr in lr_values:
+        for net_arch in net_arch_values:
+            for batch_size in batch_size_values:
+                model_name = f"PPO_lr{lr}_netarch{net_arch}_batchsize{batch_size}_timesteps{timesteps}"
+                print(f"Training {model_name}...")
+                policy_kwargs["net_arch"] = net_arch
+                model = PPO("CnnPolicy", fs_vec_env, verbose=1, policy_kwargs=policy_kwargs,
+                            learning_rate=lr, batch_size=batch_size, clip_range=0.1, ent_coef=0.01, 
+                            gae_lambda=0.9, gamma=0.99, max_grad_norm=0.5, n_epochs=4, 
+                            n_steps=128, vf_coef=0.5, device='cuda', tensorboard_log='runs/')
+                model.learn(timesteps, tb_log_name=model_name, callback=HParamCallback())
+                model.save(f"{model_name}.zip")
+                del model
+
+def efficient_net():
+    efficient_net_weights = torchvision.models.EfficientNet_B1_Weights.IMAGENET1K_V1
+    efficient_net_model = torchvision.models.efficientnet_b1(weights=efficient_net_weights).to(device)
+    efficient_net_model.classifier = nn.Identity()
+    rand_input = th.rand(1,3,224,224).to(device)
+    with th.no_grad():
+        output = efficient_net_model(rand_input)
+        output_dim = output.shape
+    num_features = output_dim[1]
+
+    return efficient_net_model, efficient_net_weights, num_features
+
+class Grayscale(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, img):
+        return img.expand(img.shape[0], 3, *img.shape[2:])
 
 class CustomCNN(BaseFeaturesExtractor):
 
@@ -57,11 +92,13 @@ class CustomCNN(BaseFeaturesExtractor):
     :param base_model: PyTorch model
     :param weights: PyTorch weights for the model
     """
-    def __init__(self, observation_space: spaces.Box, features_dim: int, base_model = None, weights = None):
+    def __init__(self, observation_space: spaces.Box, features_dim: int, base_model = None, weights = None, preprocessing_function = None):
 
         super().__init__(observation_space, features_dim)
         self.weights = weights
         self.model = base_model
+        if preprocessing_function:
+            self.preprocessing_function = preprocessing_function
 
     def forward(self, observations: th.Tensor) -> th.Tensor:
         observations = observations.to(device)
@@ -86,29 +123,16 @@ if __name__ == "__main__":
 
     policy_kwargs = dict(
         features_extractor_class=CustomCNN,
-        features_extractor_kwargs=dict(features_dim=num_features, base_model=efficient_net_model, weights=efficient_net_weights),
+        features_extractor_kwargs=dict(features_dim=num_features, base_model=efficient_net_model, weights=efficient_net_weights,
+                                       preprocessing_function=nn.Sequential(Grayscale(), efficient_net_weights.transforms())),
     )
     LOG_DIR = "./log"
-    env = gym.make("BreakoutNoFrameskip-v4")
-    env = Monitor(env, LOG_DIR)
-    vec_env = DummyVecEnv([lambda:env])
+    vec_env = make_atari_env("BreakoutNoFrameskip-v4", n_envs=4)
     fs_vec_env = VecFrameStack(vec_env, 4, channels_order='first')
 
-    lr_values = [1e-4, 1e-5, 1e-6]
-    net_arch_values = [[64, 64], [128, 128], [256, 256]]
-    batch_size_values = [16, 32, 64]
+    timesteps = 30000
+    lr_values = [2.5e-4]
+    net_arch_values = [[128, 128]]
+    batch_size_values = [128]
 
-    timesteps = 500000
-    for lr in lr_values:
-        for net_arch in net_arch_values:
-            for batch_size in batch_size_values:
-                if lr == 1e-4 and net_arch == [64, 64]: # skip already finished ones
-                    continue
-                model_name = f"PPO_lr{lr}_netarch{net_arch}_batchsize{batch_size}_timesteps{timesteps}"
-                print(f"Training {model_name}...")
-                policy_kwargs["net_arch"] = net_arch
-                model = PPO("CnnPolicy", fs_vec_env, policy_kwargs=policy_kwargs, verbose=1,
-                            learning_rate=lr, batch_size=batch_size, device='cuda', tensorboard_log='runs/')
-                model.learn(timesteps, tb_log_name=model_name, callback=HParamCallback())
-                model.save(f"{model_name}.zip")
-                del model
+    hyperparam_search(lr_values, batch_size_values, net_arch_values, policy_kwargs, timesteps)
